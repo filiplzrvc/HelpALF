ccb43a42a39e09be5d9de1fdc391e13efc2abb0a
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-05-14 02:18:47 +0200

    Refactoring code

--- src/findSpaceship.js
+++ src/findSpaceship.js
@@ -1,10 +1,18 @@
 function MapSearcher(map) {}
 MapSearcher.prototype.findSpaceship = function (map) {
-  let firstLine = map.split('\n')[0];
+  let lines = map.split('\n');
+  let firstLine = lines[0];
+  let lastLine = lines[lines.length - 1];
 
   let col = firstLine.indexOf('X');
   if (col !== -1) {
     return [col, 0];
   }
+
+  col = lastLine.indexOf('X');
+  if (col !== -1 && lines.length) {
+    return [col, lines.length - 1];
+  }
+
   return 'Spaceship lost forever.';
 };
